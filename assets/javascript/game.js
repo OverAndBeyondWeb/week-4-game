4678cf1f6c900c15973326c8a3c75559e4c1e8e3
Juan Fox <foxj0875@gmail.com>
2018-02-28 15:48:14 -0500

    all character objects made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,90 @@
-console.log('ready');
+var daenerys = {
+  name: 'Daenerys Targaryen,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'good',
+  image: 'daenerys-targaryen.jpg'
+}
+
+var jon = {
+  name: 'Jon Snow,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'good',
+  image: 'jon-snow.jpg'
+}
+
+var eddard = {
+  name: 'Eddard Stark,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'good',
+  image: 'eddard-stark.jpg'
+}
+
+var brienne = {
+  name: 'Brienne of Tarth,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'good',
+  image: 'brienne-of-tarth.jpg'
+}
+
+var bronn = {
+  name: 'Bronn,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'good',
+  image: 'bronn.jpg'
+}
+
+var cersei = {
+  name: 'Cersei Lannister,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'evil',
+  image: 'cersei-lannister.jpg'
+}
+
+var gregor = {
+  name: 'Gregor Clegane,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'evil',
+  image: 'gregor-clegane.jpg'
+}
+
+var melisandre = {
+  name: 'Cersei Lannister,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'evil',
+  image: 'cersei-lannister.jpg'
+}
+
+var cersei = {
+  name: 'Cersei Lannister,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'evil',
+  image: 'cersei-lannister.jpg'
+}
+
+var cersei = {
+  name: 'Cersei Lannister,',
+  healthPoints: 100,
+  attackPower: 8,
+  counterAttackPower: 6,
+  morality: 'evil',
+  image: 'cersei-lannister.jpg'
+}
+
