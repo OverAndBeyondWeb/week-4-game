b350605c4f6079316209e31b35915ec91943b4b1
Juan Fox <foxj0875@gmail.com>
2018-03-01 12:10:35 -0500

    finished css for all characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,113 @@
 var daenerys = {
-  name: 'Daenerys Targaryen,',
+  name: 'Daenerys Targaryen',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'good',
   image: 'daenerys-targaryen.jpg'
-}
+};
 
 var jon = {
-  name: 'Jon Snow,',
+  name: 'Jon Snow',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'good',
   image: 'jon-snow.jpg'
-}
+};
 
 var eddard = {
-  name: 'Eddard Stark,',
+  name: 'Eddard Stark',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'good',
   image: 'eddard-stark.jpg'
-}
+};
 
 var brienne = {
-  name: 'Brienne of Tarth,',
+  name: 'Brienne of Tarth',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'good',
   image: 'brienne-of-tarth.jpg'
-}
+};
 
 var bronn = {
-  name: 'Bronn,',
+  name: 'Bronn',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'good',
   image: 'bronn.jpg'
-}
+};
 
 var cersei = {
-  name: 'Cersei Lannister,',
+  name: 'Cersei Lannister',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'evil',
   image: 'cersei-lannister.jpg'
-}
+};
 
 var gregor = {
-  name: 'Gregor Clegane,',
+  name: 'Gregor Clegane',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'evil',
   image: 'gregor-clegane.jpg'
-}
+};
 
 var melisandre = {
-  name: 'Cersei Lannister,',
+  name: 'Cersei Lannister',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'evil',
   image: 'cersei-lannister.jpg'
-}
+};
 
-var cersei = {
-  name: 'Cersei Lannister,',
+var stannis = {
+  name: 'Stannis Baratheon',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'evil',
-  image: 'cersei-lannister.jpg'
-}
+  image: 'stannis-baratheon.jpg'
+};
 
-var cersei = {
-  name: 'Cersei Lannister,',
+var petyr = {
+  name: 'Petyr Baelish,',
   healthPoints: 100,
   attackPower: 8,
   counterAttackPower: 6,
   morality: 'evil',
-  image: 'cersei-lannister.jpg'
-}
+  image: 'petyr-baelish.jpg'
+};
+
+var characters = [
+  daenerys,
+  eddard,
+  jon,
+  brienne,
+  bronn,
+  cersei,
+  gregor,
+  melisandre,
+  stannis,
+  petyr,
+];
+
+characters.forEach(function(item) {
+  var character = '<li><span>' + item.name + '</span><div class="char-img"><img src="assets/images/' + item.image + '"></div><span>' + item.healthPoints + '</span></li>';
+  if (item.morality === 'good') {
+    $('#good-guys').append(character);
+  } else if (item.morality === 'evil') {
+    $('#bad-guys').append(character);
+  }
+  
+});
 
