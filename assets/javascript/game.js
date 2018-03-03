2c3797e4f8d4f98f09c0d5a6b9c991bd0c9baf52
Juan Fox <foxj0875@gmail.com>
2018-03-03 15:21:36 -0500

    character constructor is coded and all characters are initialized

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var Game = function(hero) {
   this.enemies = [];
   this.chooseEnemies();
   console.log(this);
-}
+};
 
 //Game prototype methods
 
@@ -20,150 +20,83 @@ Game.prototype.declareLoss = function() {
 }
 
 
+//Character Factory Function
 
-
-
-
-
-
-
-
-var daenerys = {
-  name: 'Daenerys Targaryen',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'good',
-  image: 'daenerys-targaryen.jpg'
+var Character = function(name, healthPoints, attackPower, counterAttackPower, morality, image) {
+    this.name = name;
+    this.healthPoints = healthPoints;
+    this.attackPower = attackPower;
+    this.counterAttackPower = counterAttackPower;
+    this.morality = morality;
+    this.image = image;
+    this.registerForGame();
 };
 
-var jon = {
-  name: 'Jon Snow',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'good',
-  image: 'jon-snow.jpg'
+//Character prototype methods and properties
+
+Character.prototype.characters = [];
+
+Character.prototype.registerForGame = function() {
+  this.characters.push(this);
 };
 
-var eddard = {
-  name: 'Eddard Stark',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'good',
-  image: 'eddard-stark.jpg'
-};
 
-var brienne = {
-  name: 'Brienne of Tarth',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'good',
-  image: 'brienne-of-tarth.jpg'
-};
+// initialize characters
 
-var bronn = {
-  name: 'Bronn',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'good',
-  image: 'bronn.jpg'
-};
+var daenerys = new Character('Daenerys Targaryen', 100, 8, 6,'good', 'daenerys-targaryen.jpg'),
+  jon = new Character('Jon Snow', 100, 8, 6,'good', 'jon-snow.jpg'),
+  eddard = new Character('Eddard Stark', 100, 8, 6,'good', 'eddard-stark.jpg'),
+  brienne = new Character('Brienne of Tarth', 100, 8, 6, 'good', 'brienne-of-tarth.jpg'),
+  bronn = new Character('Bronn', 100, 8, 6, 'good', 'bronn.jpg'),
+  cersei = new Character('Cersei Lannister', 100, 8, 6, 'evil', 'cersei-lannister.jpg'),
+  gregor = new Character('Gregor Clegane', 100, 8, 6, 'evil', 'gregor-clegane.jpg'),
+  melisandre = new Character('Melisandre', 100, 8, 6, 'evil', 'melisandre.jpg'),
+  stannis = new Character('Stannis Baratheon', 100, 8, 6, 'evil', 'stannis-baratheon.jpg'),
+  petyr = new Character('Petyr Baelish', 100, 8, 6, 'evil', 'petyr-baelish.jpg');
 
-var cersei = {
-  name: 'Cersei Lannister',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'evil',
-  image: 'cersei-lannister.jpg'
-};
 
-var gregor = {
-  name: 'Gregor Clegane',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'evil',
-  image: 'gregor-clegane.jpg'
-};
+// wait for document to load
+$(document).ready(function() {
+ 
+  // add characters to the document
+  Character.prototype.characters.forEach(function(item) {
+    var character = '<li><span>' + item.name + '</span><div class="char-img"><img src="assets/images/' + item.image + '"></div><span>' + item.healthPoints + '</span></li>';
+    if (item.morality === 'good') {
+      $('.good-guys').append(character);
+    } else if (item.morality === 'evil') {
+      $('.bad-guys').append(character);
+    }
+  });
 
-var melisandre = {
-  name: 'Melisandre',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'evil',
-  image: 'melisandre.jpg'
-};
+  //click handler to start a game
+  $('.good-guys li').click(function() {
 
-var stannis = {
-  name: 'Stannis Baratheon',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'evil',
-  image: 'stannis-baratheon.jpg'
-};
+    // What happens in the UI
+    $('.good-guys li').not($(this)).animate({opacity: 0});
 
-var petyr = {
-  name: 'Petyr Baelish',
-  healthPoints: 100,
-  attackPower: 8,
-  counterAttackPower: 6,
-  morality: 'evil',
-  image: 'petyr-baelish.jpg'
-};
+    $(this).addClass('selected');
+    $('.bad-guys').addClass('enemies-selected');
 
-var characters = [
-  daenerys,
-  eddard,
-  jon,
-  brienne,
-  bronn,
-  cersei,
-  gregor,
-  melisandre,
-  stannis,
-  petyr,
-];
+    $('.first-instruction').delay(2500).fadeOut(8000);
+    $('.second-instruction').delay(8000).fadeIn(6000);
+    $('.third-instruction').delay(20000).fadeIn(8000);
 
-characters.forEach(function(item) {
-  var character = '<li><span>' + item.name + '</span><div class="char-img"><img src="assets/images/' + item.image + '"></div><span>' + item.healthPoints + '</span></li>';
-  if (item.morality === 'good') {
-    $('.good-guys').append(character);
-  } else if (item.morality === 'evil') {
-    $('.bad-guys').append(character);
-  }
-  
+    /************ Logic *************/
+
+    //get hero name from text of first child of li that was clicked
+    var heroName = $(this).children(':first-child').text();
+
+    //creates a 1 item array then extracts the 1 item
+    var hero = Character.prototype.characters.filter(function(item) {
+      return item.name === heroName;
+    })[0];
+
+    //new Game object initiated with the hero that was clicked on
+    var game = new Game(hero);
+
+  });
 });
 
-$('.good-guys li').click(function() {
-  // What happens in the UI
-  $('.good-guys li').not($(this)).animate({opacity: 0});
 
-  $(this).addClass('selected');
-  $('.bad-guys').addClass('enemies-selected');
 
-  $('.first-instruction').delay(2500).fadeOut(8000);
-  $('.second-instruction').delay(8000).fadeIn(6000);
-  $('.third-instruction').delay(20000).fadeIn(8000);
-
-  /************ Logic *************/
-
-  //choose hero object based on text of first child of li that was clicked
-  var heroName = $(this).children(':first-child').text();
-
-  //creates a 1 item array then extracts the 1 item
-  var hero = characters.filter(function(item) {
-    return item.name === heroName;
-  })[0];
-
-  //new Game object initiated with the hero that was clicked on
-  var game = new Game(hero);
-
-});
 
